a46b6d04a2c44b8a219296d07e765a6e5bfecc4e
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-12-05 21:46:13 +0100

    update c#

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,17 +1,2 @@
-﻿using System;
-using System.Runtime.Intrinsics.Arm;
-
-namespace Calculator 
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Clear();
-            System.Console.WriteLine("Primeiro valor: ");
-            float v1 = float.Parse(Console.ReadLine());
-
-            System.Console.WriteLine(v1);
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
