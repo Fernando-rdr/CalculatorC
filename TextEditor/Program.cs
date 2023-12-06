ff6bc8a930a743f79f0965e0008f95d2001d2b22
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-12-06 14:17:46 +0100

    text edit c#

---
+++ TextEditor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class TextEditor
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            System.Console.WriteLine("O que voc deseja fazer?");
+            System.Console.WriteLine("1 - Abrir arquivo");
+            System.Console.WriteLine("2 - Criar novo arquivo");
+            System.Console.WriteLine("0 - Sair");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0 : System.Environment.Exit(0); break;
+                case 1 : Abrir(); break;
+                case 2 : Editar(); break;
+                default: Menu(); break;
+            }
+        }
+        static void Abrir()
+        {
+            
+        }
+        static void Editar() 
+        { 
+            Console.Clear();
+            System.Console.WriteLine("Digite seu texto abaixo: (ESC para sair)");
+            System.Console.WriteLine("----------------");
+            string text = "";
+
+            while(Console.ReadKey().Key != ConsoleKey.Escape)
+        }
+    }
+}
