19d21ae587f2d01f8cf1969726a26fe52a94f586
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-12-06 11:34:04 +0100

    Stopwatch c#

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Stopwath
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime++;
+            }
+        }
+    }
+}
