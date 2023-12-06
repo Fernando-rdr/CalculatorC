d7c2608d15de344b383223809da4bb08965920af
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-12-06 13:27:38 +0100

    included substring

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -7,11 +7,26 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
-        static void Start()
+
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            System.Console.WriteLine("S = Segundos - 10segundos");
+            System.Console.WriteLine("M = Minutos - 1minuto");
+            System.Console.WriteLine("Quanto tempo deseja contar");
+            System.Console.WriteLine("0 = Sair");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length-1,1));
+            int time = int.Parse(data.Substring(0, data.Length -1));
+            System.Console.WriteLine(type);
+            System.Console.WriteLine(time);
+        }
+        static void Start(int time)
+        {
+            
             int currentTime = 0;
 
             while (currentTime != time)
@@ -21,6 +36,9 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 System.Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+            Console.Clear();
+            System.Console.WriteLine("Sotpwatch finalizado...");
+            Thread.Sleep(1500);
         }
     }
 }
