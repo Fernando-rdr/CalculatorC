0fb9b0c85479d6e0b73f1e1e3a3e1f154379dfc7
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-12-07 14:46:24 +0100

    Numbers c#

---
+++ Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Numbers // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            
+            decimal valor2 = 10.25m;
+            System.Console.WriteLine(valor);
+        }
+    }
+}
