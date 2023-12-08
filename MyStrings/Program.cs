65f0fbdd790340484a0e03c8a4c90aa0dcfad0a8
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-12-08 14:22:57 +0100

    Practicing strings

---
+++ MyStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyStrings // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Digite seu nome: ");
+            var nome = Console.ReadLine();
+            System.Console.WriteLine("Digite seu sobrenome: ");
+            var sobrenome = Console.ReadLine();
+
+            if (nome == "Fernando" && sobrenome == "Rodrigues") {
+                System.Console.WriteLine("Bem vindo Fernando Rodrigues");
+            } else if (nome == "Fernando" && sobrenome != "Rodrigues") {
+                System.Console.WriteLine("Bem vindo Fernando");
+            } else if (nome != "Fernando" && sobrenome == "Rodrigues") {
+                System.Console.WriteLine("Parente?");
+            } else { 
+                System.Console.WriteLine("Ocorreu um erro");
+            }
+        }
+        
+    }
+}
